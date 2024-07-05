b1895e6c1d1e51ceb4c08c243f84062e2176bd6e
xtaodada <xtao@xtaolink.cn>
2024-07-05 21:52:51 +0800

    support gacha log

--- metadata/pool/pool.py
+++ metadata/pool/pool.py
@@ -1,7 +1,7 @@
 from metadata.pool.pool_1 import POOL_1
 from metadata.pool.pool_2 import POOL_2
 from metadata.pool.pool_3 import POOL_3
-from metadata.pool.pool_4 import POOL_4
+from metadata.pool.pool_5 import POOL_5
 
 
 def get_pool_by_id(pool_type):
@@ -11,6 +11,6 @@ def get_pool_by_id(pool_type):
         return POOL_2
     if pool_type == 3:
         return POOL_3
-    if pool_type == 4:
-        return POOL_4
+    if pool_type == 5:
+        return POOL_5
     return None
--- modules/gacha_log/const.py
+++ modules/gacha_log/const.py
@@ -1,15 +1,12 @@
-from simnet.models.genshin.wish import BannerType
+from simnet.models.zzz.wish import ZZZBannerType
 
-PAIMONMOE_VERSION = 3
-UIGF_VERSION = "v3.0"
+ZZZGF_VERSION = "v1.0"
 
 
 GACHA_TYPE_LIST = {
-    BannerType.NOVICE: "新手祈愿",
-    BannerType.PERMANENT: "常驻祈愿",
-    BannerType.WEAPON: "武器祈愿",
-    BannerType.CHARACTER1: "角色祈愿",
-    BannerType.CHARACTER2: "角色祈愿",
-    BannerType.CHRONICLED: "集录祈愿",
+    ZZZBannerType.STANDARD: "常驻调频",
+    ZZZBannerType.CHARACTER: "代理人调频",
+    ZZZBannerType.WEAPON: "音擎调频",
+    ZZZBannerType.BANGBOO: "邦布调频",
 }
 GACHA_TYPE_LIST_REVERSE = {v: k for k, v in GACHA_TYPE_LIST.items()}
