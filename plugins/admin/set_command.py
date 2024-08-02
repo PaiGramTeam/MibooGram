1841e60f6dfade780be22f0e8cbd42a4e9995a45
xtaodada <xtao@xtaolink.cn>
2024-08-02 18:39:07 +0800

    :sparkles: Support privacy policy

--- plugins/admin/set_command.py
+++ plugins/admin/set_command.py
@@ -39,6 +39,7 @@ class SetCommandPlugin(Plugin):
             BotCommand("setcookie", "添加/重设Cookie"),
             BotCommand("player", "管理用户绑定玩家"),
             BotCommand("verify", "手动验证"),
+            BotCommand("privacy", "隐私政策"),
         ]
         group_command = [
             # 通用
