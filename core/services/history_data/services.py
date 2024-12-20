8744236172f203f021b47c0f5499d694ac8a283c
xtaodada <xtao@xtaolink.cn>
2024-12-20 16:07:26 +0800

    :sparkles: support ZZZ challenge mem

--- core/services/history_data/services.py
+++ core/services/history_data/services.py
@@ -3,12 +3,14 @@ from typing import List
 
 from simnet.models.zzz.diary import ZZZDiary
 from simnet.models.zzz.chronicle.challenge import ZZZChallenge
+from simnet.models.zzz.chronicle.challenge_mem import ZZZChallengeMem
 
 from core.services.history_data.models import (
     HistoryData,
     HistoryDataTypeEnum,
     HistoryDataAbyss,
     HistoryDataLedger,
+    HistoryDataChallengeMem,
 )
 from gram_core.base_service import BaseService
 from gram_core.services.history_data.services import HistoryDataBaseServices
@@ -22,6 +24,7 @@ except ImportError:
 __all__ = (
     "HistoryDataBaseServices",
     "HistoryDataAbyssServices",
+    "HistoryDataChallengeMemServices",
     "HistoryDataLedgerServices",
 )
 
@@ -43,6 +46,27 @@ class HistoryDataAbyssServices(BaseService, HistoryDataBaseServices):
             data_id=abyss_data.season,
             time_created=datetime.datetime.now(),
             type=HistoryDataAbyssServices.DATA_TYPE,
+            data=jsonlib.loads(json_data),
+        )
+
+
+class HistoryDataChallengeMemServices(BaseService, HistoryDataBaseServices):
+    DATA_TYPE = HistoryDataTypeEnum.CHALLENGE_BOSS.value
+
+    @staticmethod
+    def exists_data(data: HistoryData, old_data: List[HistoryData]) -> bool:
+        floors = data.data.get("abyss_data", {}).get("list")
+        return any(d.data.get("abyss_data", {}).get("list") == floors for d in old_data)
+
+    @staticmethod
+    def create(user_id: int, abyss_data: ZZZChallengeMem):
+        data = HistoryDataChallengeMem(abyss_data=abyss_data)
+        json_data = data.model_dump_json(by_alias=True)
+        return HistoryData(
+            user_id=user_id,
+            data_id=abyss_data.season,
+            time_created=datetime.datetime.now(),
+            type=HistoryDataChallengeMemServices.DATA_TYPE,
             data=jsonlib.loads(json_data),
         )
 
